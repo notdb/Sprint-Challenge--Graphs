dd2e66f80ee77e06fdf5be6031091ca07c4b3b27
Doug <me@douglasbowen.com>
2019-10-11 00:16:22 -0400

    add construct_traversal to add rooms to unexplored graph

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -2,21 +2,72 @@ from room import Room
 from player import Player
 from world import World
 from roomgraphs import roomGraph0
-
+from util import Stack, Queue  # These may come in handy
 import random
 
+                    
 # Load world
 world = World()
 
 world.loadGraph(roomGraph0)
-world.printRooms()
-player = Player("Name", world.startingRoom)
+#world.printRooms()
+#print(world.rooms[1].getExits())
+#print(world.roomGrid)
 
+player = Player("Name", world.startingRoom)
+player2 = Player("Doug", world.startingRoom)
 
 # FILL THIS IN
-traversalPath = ['n', 's']
+graphlist = {}
+def construct_traversal():
+    for i in range(len(world.rooms)):
+        player2.currentRoom = world.rooms[i]
+    
+    #while len(graphlist) is not len(world.rooms):
+        unvisited = {}
+        listOfExits = player2.currentRoom.getExits()
+        for exit in listOfExits:
+            unvisited[exit] = '?'
+            graphlist[player2.currentRoom.id] = unvisited
+        print(graphlist)
+    '''
+    for rooms in graphlist:
+        listOfExits2 = []
+        print(graphlist[rooms])
+        print('?' in graphlist[rooms].values())
+        if '?' in graphlist[rooms].values():
+            for room in graphlist[rooms]:
+                print(room)
+                player2.travel(room)
+                listOfExits2 = player2.currentRoom.getExits()
+                for exit in listOfExits2:
+                    unvisited = {}
+                    unvisited[exit] = '?'
+                    graphlist[player2.currentRoom.id] = unvisited
+    '''
+## need to split up the traversal because you can't change a dictionaries size during something apparently
+## add keys and then traverse them
 
-
+           
+    
+construct_traversal()
+#print(world.rooms)
+'''
+def bft(starting_vertex):
+        que = Queue()
+        visited = set()
+        que.enqueue(starting_vertex)
+        while que.size() > 0:
+            vertex = que.dequeue()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                for next_vert in world.rooms[vertex].id:
+                    que.enqueue(next_vert)
+'''
+#bft(0)
+'''
+traversalPath = ['n', 'n','s','s','e','w']
 # TRAVERSAL TEST
 visited_rooms = set()
 player.currentRoom = world.startingRoom
@@ -30,16 +81,18 @@ if len(visited_rooms) == len(roomGraph0):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(roomGraph0) - len(visited_rooms)} unvisited rooms")
-
+'''
 
 
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-# player.currentRoom.printRoomDescription(player)
-# while True:
-#     cmds = input("-> ").lower().split(" ")
-#     if cmds[0] in ["n", "s", "e", "w"]:
-#         player.travel(cmds[0], True)
-#     else:
-#         print("I did not understand that command.")
+'''
+player.currentRoom.printRoomDescription(player)
+while True:
+    cmds = input("-> ").lower().split(" ")
+    if cmds[0] in ["n", "s", "e", "w"]:
+        player.travel(cmds[0], True)
+    else:
+        print("I did not understand that command.")
+'''
--- graph_adventure/notes.md
+++ graph_adventure/notes.md
@@ -1,2 +1,23 @@
 Work on solutions for three different room types, modifying as needed before
 working ono the final solution.
+
+So we have to fill in the traversal path somehow, we have a graph, need a way to
+test movement
+
+we also have the graph of rooms, a dictionary of rooms
+that looks like
+
+roomGraph0={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7),
+{'s': 1}]}
+
+It's a dictionary whose key is an array, with a tuple, and another dictionary
+containing valid directions
+
+It looks like what we're going to end up doing is looping through the room
+graph, and adding n, s, e ,w to the traversal path
+
+Two more questions, how do we know we've visited every room, and whats moving to
+a new room look like
+
+going to make a bft to traverse all the rooms to build up the traversalPath and
+try that. It's not going to work for every situation but it's a start
